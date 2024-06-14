9cb671553e72782f354a91825187cdcfc83e6f30
zilae <2081531631@qq.com>
2024-06-14 11:06:22 +0800

    init

---
+++ build.sh
@@ -0,0 +1,37 @@
+
+opencv_dir=$1
+install_path=$2
+echo "Configuring and building Thirdparty/DBoW2 ..."
+
+cd Thirdparty/DBoW2
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DOpenCV_DIR=${opencv_dir}
+make -j
+
+cd ../../g2o
+
+echo "Configuring and building Thirdparty/g2o ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../../
+
+echo "Uncompress vocabulary ..."
+
+cd Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
+
+echo "Configuring and building ORB_SLAM2 ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+         -DOpenCV_DIR=${opencv_dir} \
+         -DCMAKE_INSTALL_PREFIX=${install_path}
+
+make install -j
